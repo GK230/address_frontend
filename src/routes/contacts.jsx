874651ba0367d50f7bb830b9c09043f6985280cc
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-12-16 13:59:05 +0000

    fetched contacts

--- src/routes/contacts.jsx
+++ src/routes/contacts.jsx
@@ -1,7 +1,31 @@
+import useStore from "../store";
+import { useEffect } from "react";
+
 export default function Contacts() {
+  const getAllContacts = useStore((state) => state.getAllContacts);
+  const contacts = useStore((state) => state.contacts)
+
+  useEffect(() => {
+    getAllContacts();
+  }, []);
+
+
     return (
       <main>
         <h2>Contacts</h2>
+        <ul>
+          {contacts.map((contact) => (
+          <li>
+            <div className="contact">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+              <p>{contact.phone}</p>
+              <p>{contact.address}</p>
+            </div>
+          </li>
+          ))}
+        </ul>
+
       </main>
     );
   }
--- src/store.js
+++ src/store.js
@@ -1,23 +1,25 @@
-import create from "zustand"
+import create from "zustand";
 
 const useStore = create((set, get) => ({
-     contacts: [],
+  contacts: [],
 
-     addContact: (data) => {
-        fetch("http://localhost:5000/contact/add", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        })
-          .then((resp) => resp.json())
-          .then((newContact) => set({ contacts: [...get().contacts, newContact] }));
+  addContact: (data) => {
+    fetch("http://localhost:5000/contact/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify(data),
+    })
+      .then((resp) => resp.json())
+      .then((newContact) => set({ contacts: [...get().contacts, newContact] }));
+  },
 
-}))
+  getAllContacts: () => {
+    fetch("http://localhost:5000/contact")
+      .then((resp) => resp.json())
+      .then((contacts) => set({ contacts: contacts }));
+  },
+}));
 
-export default useStore
-
-
-
+export default useStore;
